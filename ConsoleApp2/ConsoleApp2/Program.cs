c887900014b905a8e4f01dd7f6437e935df7d293
Simon Myrvig <simo008s@mail.hansenberg.dk>
2024-08-15 08:32:24 +0200

    Fibonacci

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -5,6 +5,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        static long Fibonacci(int i)
+        {
+            if (i == 0) return 0;
+            if (i == 1) return 1;
+            if (i == 2) return 1;
+            return Fibonacci(i - 1) + Fibonacci(i - 2);
+            // return 0; dummy
+
+        }
         Stopwatch stopWatch = new Stopwatch();
         stopWatch.Start();
         Thread.Sleep(10000);
